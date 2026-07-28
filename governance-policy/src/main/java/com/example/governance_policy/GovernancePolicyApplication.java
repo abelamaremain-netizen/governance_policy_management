e77f8a3585e3b89764d4bc5ec026294816af1343
abelabel22 <abmarmaru@gmail.com>
2026-07-28 23:07:06 +0300

    the projects initial configuration setup, the spring boot is npw up and running

---
+++ governance-policy/src/main/java/com/example/governance_policy/GovernancePolicyApplication.java
@@ -0,0 +1,13 @@
+package com.example.governance_policy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernancePolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernancePolicyApplication.class, args);
+	}
+
+}
---
+++ governance-policy/src/test/java/com/example/governance_policy/GovernancePolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.governance_policy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernancePolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
